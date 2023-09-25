2a1830ea3ad49c817e665e96bcd1a1594a03b5f7
Dmssnnn <129981482+Dmssnnn@users.noreply.github.com>
2023-09-25 16:34:39 +0400

    update

--- script.js
+++ script.js
@@ -17,36 +17,38 @@ function getTimeAndDay() {
   function getInputResults() {
     const button = document.getElementById("button");
     const getInputValues = document.getElementById("getInputValues");
-
+  
     button.addEventListener("click", function () {
-
       const para = document.createElement("div");
       para.classList.add("input-para");
-
-      const hours = new Date().getHours().toString()
-      const minutes = new Date().getMinutes().toString()
+  
+      const hours = new Date().getHours().toString();
+      const minutes = new Date().getMinutes().toString();
       const input = document.getElementById("input").value;
-
+  
       para.innerHTML = `
         <div class="divi">
           <div class="text-wrap">
             <span>${input}</span>
             <h6>Today At ${hours}:${minutes}</h6>
           </div>
-          <div><button onclick="deleteRaw(this)" class="rubbish-button"><img src="imgs/rubbish.svg" alt=""></button></div>
+          <div><button class="rubbish-button"><img src="imgs/rubbish.svg" alt=""></button></div>
         </div>
       `;
-    
+  
       getInputValues.appendChild(para);
-      document.getElementById("input").value = ''; 
+      document.getElementById("input").value = "";
+
+      const rubbishButton = para.querySelector(".rubbish-button")
+      rubbishButton.addEventListener("click" , function ()  {
+
+        para.remove();
+  
+      })
     });
   }
-
+  
   getInputResults();
 
-  function deleteRaw(item) {
-    const para = item.parentElement.parentElement;
-    para.remove();
-  }
 
-
+  
